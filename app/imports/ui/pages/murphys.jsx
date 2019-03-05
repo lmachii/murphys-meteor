63f01cca318490e89c23f66c2168d5e212f08c32
Leland Machii <lelandmachii@gmail.com>
2019-03-05 11:08:03 -1000

    Final version

---
+++ app/imports/ui/pages/murphys.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Middle from '../components/Middle.jsx';
+
+export default class IslandSnow extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Middle/>
+        </div>
+    );
+  }
+}
